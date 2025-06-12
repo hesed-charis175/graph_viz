a7a8ee6cd28901ed6bc03e12f1bd80f652d02201
hesed-charis175 <bayiha.charis@ictuniversity.edu.cm>
2025-06-12 18:53:24 +0100

    Create ui_core.h

---
+++ src/ui_core.h
@@ -0,0 +1,44 @@
+#include "imgui.h"
+#include "config.h"
+#include <iostream>
+#include "namespace.h"
+#include <string>
+#include <chrono>
+#include <deque>
+#include <vector>
+
+
+struct KeyBufferEntry{
+    ImGuiKey key;
+    std::string name;
+    std::chrono::steady_clock::time_point timestamp;
+};
+
+
+static std::deque<KeyBufferEntry> s_KeyBuffer;
+static constexpr float s_multiKeyDuration = 0.3f;
+static constexpr float s_uniqueKeyDuration = 1.0f;
+
+static bool s_CopiedOnceFlag = false;
+
+
+static std::vector<Node*> s_selectedNodes;
+
+#define IM_MIN(A, B)            (((A) < (B)) ? (A) : (B))
+static bool s_configOpen = NULL;
+
+static Node* s_targetEditable = nullptr;
+
+struct WindowData{
+    bool showMainMenuBar = true;
+    bool showAppSimpleOverlay = true;
+    bool showMetrics = false;
+    bool showAbout = false;
+    bool showStyleEditor = false;
+    bool showNodeEditor = false;
+    bool evaluateClick = true;
+    bool updateNodes = true;
+    bool editableNode = false;
+};
+
+static Node s_copiedNode;
