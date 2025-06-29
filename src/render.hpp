4a37fe97e5233b268d1bed579cf366d0c7c12d76
hesed-charis175 <bayiha.charis@ictuniversity.edu.cm>
2025-06-29 20:02:45 +0100

    Update render.hpp

--- src/render.hpp
+++ src/render.hpp
@@ -5,6 +5,7 @@
 #include "draw_node.hpp"
 #include "node_editor.hpp"
 #include "node_rendering.hpp"
+#include "viewport.hpp"
 
 static void showWindowMenuBar(WindowData* winData)
 {
@@ -27,14 +28,67 @@ static void showWindowMenuBar(WindowData* winData)
     }
 }
 
+void showPanel(WindowData* winData){
+    const ImGuiViewport* mainViewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(ImVec2(mainViewport->WorkPos.x + 20, mainViewport->WorkPos.y + 20), ImGuiCond_Once);
+    ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
+
+    if(!ImGui::Begin("Panel", NULL, ImGuiWindowFlags_MenuBar)){
+        ImGui::End();
+        return;
+    }
+
+    const float labelWidthBase = ImGui::GetFontSize() * 12;
+    const float labelWidthMax = ImGui::GetContentRegionAvail().x * 0.40f;
+    const float labelWidth = IM_MIN(labelWidthBase, labelWidthMax);
+    ImGui::PushItemWidth(-labelWidth);
+
+    showWindowMenuBar(&s_winData);
+
+    ImGui::Text("Graph Viewer v(%s)", Strings::version);
+    ImGui::Spacing();
+
+    if(ImGui::BeginTabBar("Panel", ImGuiTabBarFlags_None)){
+        if (ImGui::BeginTabItem("Style")){
+            ImGui::Text("This is the Style Editor Tab. It controls the appearance.\n");
+            ImGui::Spacing();
+            ImGui::Spacing();
+            showStyleEditor(winData);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Map"))
+            {
+                ImGui::Text("This is the Broccoli tab!\nblah blah blah blah blah");
+                ImGui::EndTabItem();
+            }
+
+        ImGui::EndTabBar();
+    }
+
+    ImGui::Spacing();
+    ImGui::Spacing();
+
+
+    ImGui::End();
+}
+
+
+
 void graphVisualizerRender(bool* p_open){
+    Config::windowHeight = ImGui::GetMainViewport()->Size.y;
+    Config::windowWidth = ImGui::GetMainViewport()->Size.x;
+
+
+    if(Config::showGrids) {showGrid(&s_winData); }
     if(s_winData.showMainMenuBar) showAppMainMenuBar(&s_winData);
     if(s_winData.showStyleEditor) showStyleEditor(&s_winData);
     if(s_winData.showAppSimpleOverlay) showAppSimpleOverlay(&s_winData);
     if(s_winData.showAbout) showAbout(&s_winData);
     if (s_winData.evaluateClick) handleClickInViewport(&s_winData);
-    if (s_winData.updateNodes) updateNodes(&s_winData);
-    if (s_winData.showNodeDebugList) showDebugNodeList(&s_winData);
+    if (s_winData.updateNodes) {updateEdges(&s_winData); updateNodes(&s_winData); }
+    if (s_winData.showNodeDebugList) showDebugNodeList(&s_winData, [&](){
+        if(s_targetEditable) showNodeEditor(&s_winData, s_targetEditable, true);
+    });
     if (s_winData.showDebugLogs) showDebugLogs(&s_winData);
     
     ImGuiIO& io = ImGui::GetIO();
@@ -57,33 +111,6 @@ void graphVisualizerRender(bool* p_open){
     }else{
         s_selectedNodes.clear();
     }
-            
-    
-    const ImGuiViewport* mainViewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(ImVec2(mainViewport->WorkPos.x + 20, mainViewport->WorkPos.y + 20), ImGuiCond_Once);
-    ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
 
-    if(!ImGui::Begin("Panel", NULL, ImGuiWindowFlags_MenuBar)){
-        ImGui::End();
-        return;
-    }
-
-    const float labelWidthBase = ImGui::GetFontSize() * 12;
-    const float labelWidthMax = ImGui::GetContentRegionAvail().x * 0.40f;
-    const float labelWidth = IM_MIN(labelWidthBase, labelWidthMax);
-    ImGui::PushItemWidth(-labelWidth);
-
-    showWindowMenuBar(&s_winData);
-
-    ImGui::Text("Graph Viewer v(%s)", Strings::version);
-    ImGui::Spacing();
-
-    if(ImGui::Button("Appearance Editor")){
-        s_winData.showStyleEditor = true;
-    }
-    ImGui::Spacing();
-    ImGui::Spacing();
-
-
-    ImGui::End();
+    showPanel(&s_winData);
 }
